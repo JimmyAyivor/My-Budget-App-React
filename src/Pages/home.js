ec1eb4a6bcaa0f028c79dcb0e8280e296aaef2aa
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2022-11-12 13:42:06 -0500

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Typography } from "@mui/material";
 
 const home = () => {
-  return (
+  return (<>
     <Typography variant='h4' justifyContent='center'>
       {" "}
       Welcome to My Budgeting App{" "}
     </Typography>
 <Typography variant='p'>A Budgeting App to create, update, and delete budget transactions such as income, taxes, expenses and savings etc…</Typography>
 <Typography variant='p'>Built with HTML, CSS, JavaScript,Node.js, React, Express, Material UI with  backend deployed on Heroku and frontend on Netlify.</Typography>
+</>
   );
 };
 
