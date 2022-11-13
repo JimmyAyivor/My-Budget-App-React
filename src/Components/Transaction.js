3e555d735c2bdbe16a4cc1f91a9dc93f82c96806
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2022-11-13 08:38:58 -0500

    Update Transaction.js

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -32,9 +32,9 @@ const transactionTable = ( <TableRow hover role='checkbox' tabIndex={-1} key={in
       </TableCell>
       <TableCell align='right'>{transaction.amount}</TableCell>
     </TableRow>)
-  return ({row ? transactionTable : 'No transactions found'}
+  return (<>{row ? transactionTable : 'No transactions found'}
    
-  );
+  </>);
 };
 
 export default Transaction;
